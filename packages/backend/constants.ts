17027b7b334b8a1d1dfbd88df576ff6d1e0df7c9
Julio Alcantara <jalcantara88@gmail.com>
2021-05-13 08:10:52 -0700

    added github actions to deploy to heroku

--- packages/backend/constants.ts
+++ packages/backend/constants.ts
@@ -1,1 +1,4 @@
-export const mongoConnectionString = "mongodb://localhost/twitterbean";
+//export const mongoConnectionString = "mongodb://localhost/twitterbean";
+
+
+export const mongoConnectionString = "mongodb+srv://admin:09PVSRAKdneL6Ge7@cluster0.zu8gt.mongodb.net/beansTalk?retryWrites=true&w=majority";
